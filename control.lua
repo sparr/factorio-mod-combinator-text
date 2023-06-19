6626b0923c5d2828a6d9630af75c200be1d170c6
Clarence "Sparr" Risher <sparr0@gmail.com>
2023-06-18 21:33:00 -0400

    Add doc comments
    Signed-off-by: Clarence "Sparr" Risher <sparr0@gmail.com>

--- control.lua
+++ control.lua
@@ -1,10 +1,13 @@
--- get the text from an existing combinator
+---get the text from an existing combinator
+---@param combinator LuaEntity?
+---@return string
 local function get_combinator_text(combinator)
     local text = ""
     if combinator and combinator.valid then
-        local control = combinator.get_control_behavior()
+        local control = combinator.get_control_behavior() --[[@as LuaConstantCombinatorControlBehavior]]
         if control and control.valid then
             local skip = 0
+            ---@type uint
             for i = 1, control.signals_count do
                 local signal = control.get_signal(i).signal
                 local c = nil
@@ -25,29 +28,34 @@ local function get_combinator_text(combinator)
     return text
 end
 
--- when opening a constant combinator, create and populate the mod gui
+---when opening a constant combinator, create and populate the mod gui
+---@param event EventData.on_gui_opened
 local function on_gui_opened(event)
     local entity = event.entity
     if not entity or entity.type ~= "constant-combinator" then return end
     local player = game.get_player(event.player_index)
+    if not player then return end
 
     -- destroy the frame if it still exists from previously
     local relative_frame = player.gui.relative["combinator-text"]
     if relative_frame then relative_frame.destroy() end
 
     -- anchor the frame below the constant combinator gui
+    ---@type GuiAnchor
     local anchor = {gui=defines.relative_gui_type.constant_combinator_gui, position=defines.relative_gui_position.bottom}
     -- create the frame
+    ---@type LuaGuiElement
     local frame = player.gui.relative.add{type="frame", anchor=anchor, name="combinator-text", caption="Combinator Text", direction="vertical"}
     frame.add{type="textfield", name="textfield", text=get_combinator_text(entity)}
     frame.add{type="button", name="button", caption="Apply"}
 end
 
--- use the text in a player's mod gui to set the signals in their open combinator
+---use the text in a player's mod gui to set the signals in their open combinator
+---@param event EventData.on_gui_click | EventData.on_gui_confirmed
 local function on_gui_apply(event)
     local element = event.element
     if
-        element.parent.name == "combinator-text" and
+        element.valid and element.parent and element.parent.name == "combinator-text" and
         (
             (event.name == defines.events.on_gui_click and element.type == "button") or
             (event.name == defines.events.on_gui_confirmed and element.type == "textfield")
@@ -55,8 +63,9 @@ local function on_gui_apply(event)
     then
         local combinator = game.get_player(event.player_index).opened
         local text = element.parent["textfield"].text
-        if combinator.valid then
-            local control = combinator.get_control_behavior()
+        if combinator and combinator.valid then
+            local control = combinator.get_control_behavior() --[[@as LuaConstantCombinatorControlBehavior]]
+            ---@type uint
             for i = 1, math.min(#text, control.signals_count)  do
                 local signal = "signal-" .. text:sub(i,i):upper()
                 if game.virtual_signal_prototypes[signal] then
